8e40d0840f43f5502a63559e24ae6a156cb02a97
florin <florinasca@mail.ru>
2025-05-06 16:01:57 +0300

    Initial commit.

--- src/main/java/pixel/academy/crud_app/CrudAppApplication.java
+++ src/main/java/pixel/academy/crud_app/CrudAppApplication.java
@@ -21,14 +21,14 @@ public class CrudAppApplication {
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
 //			createStudent(studentDAO);
-//			createMultipleStudents(studentDAO);
+			createMultipleStudents(studentDAO);
 //			readStudent(studentDAO);
 //			queryForStudents(studentDAO);
 //			queryForStudentsByLastName(studentDAO);
 //			updateStudent(studentDAO);
 //			deleteStudent(studentDAO);
 
-			deleteAllStudents(studentDAO);
+			//deleteAllStudents(studentDAO);
 
 		};
 	}
